feedab862d5689d54783016be453caf6e2c4ab8f
PrsnVkngs <oskaraattar@gmail.com>
2025-11-18 10:22:08 -0800

    exp1 fix

--- exp1.py
+++ exp1.py
@@ -1,14 +1,13 @@
 from mininet.net import Mininet
-from mininet.node import Node, Host
-from mininet.link import Link, TCLink
-from mininet.log import setLogLevel, info
+from mininet.node import Node
+from mininet.link import TCLink
+from mininet.log import setLogLevel
 from mininet.cli import CLI
 
 class LinuxRouter(Node):
     def config(self, **params):
         super().config(**params)
         self.cmd("sysctl -w net.ipv4.ip_forward=1")
-
     def terminate(self):
         self.cmd("sysctl -w net.ipv4.ip_forward=0")
         super().terminate()
@@ -21,65 +20,62 @@ def run():
     r2 = net.addHost('r2', cls=LinuxRouter)
 
     # Hosts
-    h1 = net.addHost('h1', ip='10.0.0.1/24')
-    h2 = net.addHost('h2', ip='10.0.3.2/24')
-    h3 = net.addHost('h3', ip='10.0.2.2/24')
+    h1 = net.addHost('h1')  # IPs set later per-link
+    h2 = net.addHost('h2')
+    h3 = net.addHost('h3')
 
-    # Links + interface IPs on routers
-    net.addLink(h1, r1)
-    r1.setIP('10.0.0.3/24', intf=r1.defaultIntf()) 
-
-    # r1 <-> r2
-    L = net.addLink(r1, r2)
-    intf_r1_r2 = r1.nameToIntf(L.intf1.name)
-    intf_r2_r1 = r2.nameToIntf(L.intf2.name)
-    r1.setIP('10.0.1.1/24', intf=intf_r1_r2)
-    r2.setIP('10.0.1.2/24', intf=intf_r2_r1)
-
-    # r2 <-> h3
-    net.addLink(r2, h3)
-    # set r2 IP on the h3-facing interface
-    r2_intfs = [i for i in r2.intfList() if i != r2.defaultIntf() and i != intf_r2_r1]
-    r2_to_h3 = [i for i in r2_intfs if r2.IP(i) == '0.0.0.0'][0]
-    r2.setIP('10.0.2.1/24', intf=r2_to_h3)
-
-    # r1 <-> h2
-    net.addLink(r1, h2)
-    # set r1 IP on the h2-facing interface
-    r1_intfs = [i for i in r1.intfList() if i not in (r1.defaultIntf(), intf_r1_r2)]
-    r1_to_h2 = [i for i in r1_intfs if r1.IP(i) == '0.0.0.0'][0]
-    r1.setIP('10.0.3.4/24', intf=r1_to_h2)
+    # Links (keep references to both ends)
+    L_h1_r1 = net.addLink(h1, r1)      # h1-eth0 <-> r1-eth?
+    L_r1_r2 = net.addLink(r1, r2)      # r1-eth? <-> r2-eth?
+    L_r2_h3 = net.addLink(r2, h3)      # r2-eth? <-> h3-eth0
+    L_r1_h2 = net.addLink(r1, h2)      # r1-eth? <-> h2-eth0
 
     net.build()
 
-    # Host default routes to their first-hop routers
+    # h1—r1: 10.0.0.1/24 (h1) ↔ 10.0.0.3/24 (r1)
+    h1.setIP('10.0.0.1/24', intf=L_h1_r1.intf1)
+    r1.setIP('10.0.0.3/24', intf=L_h1_r1.intf2)
+
+    # r1—r2: 10.0.1.1/24 (r1) ↔ 10.0.1.2/24 (r2)
+    r1.setIP('10.0.1.1/24', intf=L_r1_r2.intf1)
+    r2.setIP('10.0.1.2/24', intf=L_r1_r2.intf2)
+
+    # r2—h3: 10.0.2.1/24 (r2) ↔ 10.0.2.2/24 (h3)
+    r2.setIP('10.0.2.1/24', intf=L_r2_h3.intf1)
+    h3.setIP('10.0.2.2/24', intf=L_r2_h3.intf2)
+
+    # r1—h2: 10.0.3.4/24 (r1) ↔ 10.0.3.2/24 (h2)
+    r1.setIP('10.0.3.4/24', intf=L_r1_h2.intf1)
+    h2.setIP('10.0.3.2/24', intf=L_r1_h2.intf2)
+
+    # Default routes on hosts
     h1.cmd("ip route add default via 10.0.0.3")
     h2.cmd("ip route add default via 10.0.3.4")
     h3.cmd("ip route add default via 10.0.2.1")
 
-    # Router static routes:
-    # r1 needs to reach 10.0.2.0/24 via r2
+    # Static routes on routers
     r1.cmd("ip route add 10.0.2.0/24 via 10.0.1.2")
-    # r2 needs to reach 10.0.0.0/24 and 10.0.3.0/24 via r1
     r2.cmd("ip route add 10.0.0.0/24 via 10.0.1.1")
     r2.cmd("ip route add 10.0.3.0/24 via 10.0.1.1")
 
-    # Pings to collect into result1.txt
-    def run_ping(src, dst):
-        return net.get(src).cmd(f"ping -c 1 {dst}; echo $?")
+    # for n in (h1, h2, h3, r1, r2):
+    #     print(n.name, n.cmd("ip -br addr"))
+
+    # Run pings and write result1.txt
+    def ping(src, dst):
+        return net.get(src).cmd(f"ping -c 1 {dst}")
 
     with open("result1.txt", "w") as f:
         f.write("# h1 -> h3\n")
-        f.write(run_ping("h1", "10.0.2.2"))
-        f.write("\n# h2 -> h3\n")
-        f.write(run_ping("h2", "10.0.2.2"))
-        f.write("\n# h3 -> h1\n")
-        f.write(run_ping("h3", "10.0.0.1"))
-        f.write("\n# h3 -> h2\n")
-        f.write(run_ping("h3", "10.0.3.2"))
-    
+        f.write(ping("h1", "10.0.2.2") + "\n")
+        f.write("# h2 -> h3\n")
+        f.write(ping("h2", "10.0.2.2") + "\n")
+        f.write("# h3 -> h1\n")
+        f.write(ping("h3", "10.0.0.1") + "\n")
+        f.write("# h3 -> h2\n")
+        f.write(ping("h3", "10.0.3.2") + "\n")
+
     net.stop()
-    
 
 if __name__ == "__main__":
     setLogLevel('info')
